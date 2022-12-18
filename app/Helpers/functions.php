dbdf24815b83c3ec9e87076e20be77b65c2023e3
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2022-12-18 17:52:00 -0300

    Acionado arquivo de helpers para ajudar com funções globais

---
+++ app/Helpers/functions.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Remove qualquer caracter da string deixando apenas números
+ *
+ * @param string $value
+ * @return string
+ */
+function onlyNumbers(string $value): string
+{
+    return preg_replace('/[^0-9]/i', '', $value);
+}
+
+function maskInput($type, $value): string
+{
+    switch($type){
+        case "cpf":
+            return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/i", "$1.$2.$3-$4", $value);
+        break;
+
+        default:
+            return $value;
+        break;
+    }
+}
