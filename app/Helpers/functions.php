33068c06aa82c1bae51a4253748eca749f305843
Alexandre Soffiatti <alexandresofiati@hotmail.com>
2022-12-18 23:20:01 -0300

    Mascara para telefones

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -18,6 +18,10 @@ function maskInput($type, $value): string
             return preg_replace("/([0-9]{3})([0-9]{3})([0-9]{3})([0-9]{2})/i", "$1.$2.$3-$4", $value);
         break;
 
+        case "phone_number":
+            return preg_replace('/([0-9]{2})([0-9]{4,5})([0-9]{4})/i', '($1) $2-$3', $value);
+        break;
+
         default:
             return $value;
         break;
